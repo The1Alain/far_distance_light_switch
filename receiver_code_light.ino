128b52d4e55b5faae4db02dfd54941c2a1da86f1
The1Alain <alainchidiac23@gmail.com>
2026-06-06 11:02:15 +0400

    Refactor comments for better clarity and translation
    Updated comments for clarity and corrected some French translations.

--- receiver_code_light.ino
+++ receiver_code_light.ino
@@ -1,7 +1,11 @@
+// Inclusion des bibliotèques requises.
+// Inclusion of the required libraries.
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <Servo.h>
+// Mise en place des servomotteurs and des variables utilisée
+// Setup of the servo motors and used variables 
 Servo monServo;  
 Servo monServo_2;
 int servo_position = 0;
@@ -13,7 +17,8 @@ boolean buttonState_2 = 0;
 
 void setup() {
   pinMode(2, OUTPUT);
-  
+  // Initalisation des modules NRF 24L01
+  // Setup of the NRF 24L01 modules
   radio.begin();
   radio.openWritingPipe(addresses[1]); 
   radio.openReadingPipe(1, addresses[0]); 
@@ -31,14 +36,17 @@ void loop() {
   int potValue = analogRead(A0);
   int angleValue = map(potValue, 0, 1023, 0, 180);
   radio.write(&angleValue, sizeof(angleValue));
-
-
+  // The NRF 24L01 modules start to receive the states of the buttons on the remote controls
+  // Les modules NRF 24L01 commencent à recevoir les états des boutons sur la télécommande
   radio.startListening();
   radio.read(&buttonState, sizeof(buttonState));;
-  
+  // Algorithm that turns the servo motors depending on the state of the button
+  // Algorithme qui tourne les servomotteurs dépendament de l'état du boutton
   if (buttonState == HIGH) {
     servo_position = 70; // angle to change if you need to / angle à modifier si vous avez besoin
     monServo.write(servo_position);
+    // L'arrêt permet de donner le temps au servomotteur de tourner complètement avant que l'état du boutton change
+    // The delay gives time to the servo motor to turn completely before the state of the button changes
     delay(2000);
     
   }
@@ -58,6 +66,8 @@ void loop() {
     servo_position_2 = 0 ; // angle to change if you need to / angle à modifier si vous avez besoin
     monServo_2.write(servo_position_2);
   }
+  // This code was used for debugging and setting a good delay since depending on the distance of the NRF 24L01 modules the delay would need to change.
+  // Ces lignes ont été utilisé pour le déboggage et paramettrer un bonne arrêt plus que dépendement de la distances des models NRF 24L01 modules le temp d'arrêt devrait changer..
   Serial.println(buttonState);
   Serial.println(buttonState_2);
   Serial.println(servo_position);
